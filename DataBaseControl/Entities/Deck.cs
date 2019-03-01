c9bdb2643843f6f233eb96eb9ee33c72a9890284
Anuitex - 13 <Anuitex - 13@DESKTOP-QE6PMV6>
2019-03-01 10:44:31 +0200

    GameStart

---
+++ DataBaseControl/Entities/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DataBaseControl.Entities
+{
+    public class Deck
+    {
+        public List<Card> Cards { get; set; }
+        public int NumberCard { get; set; }
+
+        public Deck ()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 2; j < 15; j++)
+                {
+                    Cards.Add(
+                        new Card
+                        {
+                            LearCard = (Enam.LearCard)i,
+                            NumberCard = (Enam.NumberCard)j
+                        });
+                }
+            }
+            NumberCard = 52;
+        }
+    }
+}
