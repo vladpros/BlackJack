7d3e1fbc2ade3478c83a61fa3b8da7c345cd89dd
Anuitex - 13 <Anuitex - 13@DESKTOP-QE6PMV6>
2019-03-25 17:41:30 +0200

    name change

--- BlackJack.Api/UI/src/app/sheared/models/card-view.ts
+++ BlackJack.Api/UI/src/app/sheared/models/card-view.ts
@@ -2,6 +2,6 @@ import { CardNumber } from '../enams/card-number.enum';
 import { CardLear } from '../enams/card-lear.enum';
 
 export interface CardView {
-  cardLeard: CardLear;
+  cardLear: CardLear;
   cardNumber: CardNumber;
 }
